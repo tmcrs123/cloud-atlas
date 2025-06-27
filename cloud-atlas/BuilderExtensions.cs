b8ee3081df47e0729f441fcf67f2f13a008b8003
Tiago Rodrigues <tmcrs123@gmail.com>
2025-06-27 16:06:23 +0100

    ef core migrations via docker and more scafolding

---
+++ cloud-atlas/BuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using cloud_atlas.Shared.Exceptions;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace cloud_atlas
+{
+    public static class BuilderExtensions
+    {
+        public static void ConfigureGlobalErrorHandling(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddProblemDetails();
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        }
+
+        public static void ConfigureCors(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy
+                    .WithOrigins(["*"])
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .AllowAnyMethod();
+                });
+            });
+        }
+
+        public static void ConfigureResponseCompression(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes;
+            });
+        }
+
+    }
+}
---
+++ cloud-atlas/Local_Dev_Tools/init.sh
@@ -0,0 +1,2 @@
+﻿#!/bin/sh
+echo "bananas" && sleep 3600 && /opt/mssql-tools/bin/sqlcmd -S \"$SQL_HOST\" -U \"$SQL_USER\" -P \"SQL_PASSWORD\" -i /tmp/seed.sql && echo "Done."
